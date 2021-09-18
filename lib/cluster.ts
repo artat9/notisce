dec32dce3915bb551f6456a7efa403e673ce9064
atatur9 <momodarou2015@gmail.com>
2021-09-18 16:29:48 +0900

    to ecs from eks

--- lib/cluster.ts
+++ lib/cluster.ts
@@ -1,22 +1,15 @@
+import { SubnetType, Vpc } from "@aws-cdk/aws-ec2";
 import {
-  InstanceClass,
-  InstanceSize,
-  InstanceType,
-  SubnetType,
-  Vpc,
-} from "@aws-cdk/aws-ec2";
-import { Repository, TagMutability } from "@aws-cdk/aws-ecr";
-import { Cluster, FargateCluster, KubernetesVersion } from "@aws-cdk/aws-eks";
-import { ManagedPolicy, Role, ServicePrincipal, User } from "@aws-cdk/aws-iam";
+  Cluster,
+  ContainerImage,
+  FargateService,
+  FargateTaskDefinition,
+  LogDriver,
+} from "@aws-cdk/aws-ecs";
+import { ManagedPolicy, Role, ServicePrincipal } from "@aws-cdk/aws-iam";
+import { LogGroup } from "@aws-cdk/aws-logs";
 import * as cdk from "@aws-cdk/core";
 import { RemovalPolicy } from "@aws-cdk/core";
-import {
-  AwsCustomResource,
-  AwsCustomResourcePolicy,
-} from "@aws-cdk/custom-resources";
-import * as cdk8s from "cdk8s";
-import { NotisceChart } from "./charts/chart";
-import { basicPolicytStatements } from "./restapi";
 
 export class ClusterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -47,104 +40,150 @@ export class ClusterStack extends cdk.Stack {
         },
       ],
     });
-
-    const eksRole = new Role(this, "eksRole", {
-      assumedBy: new ServicePrincipal("eks.amazonaws.com"),
-    });
-    eksRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSClusterPolicy")
-    );
-    eksRole.addManagedPolicy(
-      ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSServicePolicy")
-    );
-    basicPolicytStatements(this.region, this.account).forEach((s) =>
-      eksRole.addToPolicy(s)
-    );
-    new Repository(this, "notisce-repository", {
-      imageScanOnPush: true,
-      imageTagMutability: TagMutability.MUTABLE,
-      repositoryName: "notisce",
-      removalPolicy: RemovalPolicy.DESTROY,
-    });
-    const cluster = new Cluster(this, "cluster", {
-      vpc,
-      mastersRole: eksRole,
-      clusterName: "notisce-subscriber",
-      version: KubernetesVersion.V1_21,
-      role: eksRole,
-      defaultCapacity: 1,
-      defaultCapacityInstance: InstanceType.of(
-        InstanceClass.T2,
-        InstanceSize.MEDIUM
-      ),
-      vpcSubnets: [
-        {
-          subnets: vpc.privateSubnets,
-        },
+    const executionRole = new Role(this, "EcsTaskExecutionRole", {
+      roleName: "notisce-ecs-task-execution-role",
+      assumedBy: new ServicePrincipal("ecs-tasks.amazonaws.com"),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AmazonECSTaskExecutionRolePolicy"
+        ),
       ],
     });
-    cluster.addCdk8sChart(
-      "chart",
-      new NotisceChart(new cdk8s.App(), "notisce-chart")
-    );
-    const admin = User.fromUserName(this, "masterUser", "yushi");
-    cluster.awsAuth.addUserMapping(admin, { groups: ["system:masters"] });
+    const serviceTaskRole = new Role(this, "EcsServiceTaskRole", {
+      roleName: "notisce-ecs-service-task-role",
+      assumedBy: new ServicePrincipal("ecs-tasks.amazonaws.com"),
+    });
+    const logGroup = new LogGroup(this, "ServiceLogGroup", {
+      logGroupName: "/aws/ecs/notisce-cluster",
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+    const image = ContainerImage.fromAsset(".");
+    const cpu = 256;
+    const mem = 1024;
+    const taskDef = new FargateTaskDefinition(this, "ServiceTaskDefinition", {
+      cpu: cpu,
+      memoryLimitMiB: mem,
+      executionRole: executionRole,
+      taskRole: serviceTaskRole,
+    });
+    taskDef.addContainer("ContainerDef", {
+      image,
+      cpu: cpu,
+      memoryLimitMiB: mem,
+      logging: LogDriver.awsLogs({
+        streamPrefix: "notisce",
+        logGroup,
+      }),
+    });
+    const cluster = new Cluster(this, "notisce-cluster", {
+      clusterName: "notisce-cluster",
+      containerInsights: true,
+      vpc: vpc,
+      enableFargateCapacityProviders: true,
+    });
+    const fargateService = new FargateService(this, "FargateService", {
+      cluster,
+      vpcSubnets: vpc.selectSubnets({ subnetType: SubnetType.PRIVATE }),
+      taskDefinition: taskDef,
+      desiredCount: 1,
+    });
+    //const eksRole = new Role(this, "eksRole", {
+    //  assumedBy: new ServicePrincipal("eks.amazonaws.com"),
+    //});
+    //eksRole.addManagedPolicy(
+    //  ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSClusterPolicy")
+    //);
+    //eksRole.addManagedPolicy(
+    //  ManagedPolicy.fromAwsManagedPolicyName("AmazonEKSServicePolicy")
+    //);
+    //basicPolicytStatements(this.region, this.account).forEach((s) =>
+    //  eksRole.addToPolicy(s)
+    //);
+    //new Repository(this, "notisce-repository", {
+    //  imageScanOnPush: true,
+    //  imageTagMutability: TagMutability.MUTABLE,
+    //  repositoryName: "notisce",
+    //  removalPolicy: RemovalPolicy.DESTROY,
+    //});
+    //const cluster = new Cluster(this, "cluster", {
+    //  vpc,
+    //  mastersRole: eksRole,
+    //  clusterName: "notisce-subscriber",
+    //  version: KubernetesVersion.V1_21,
+    //  role: eksRole,
+    //  defaultCapacity: 1,
+    //  defaultCapacityInstance: InstanceType.of(
+    //    InstanceClass.T2,
+    //    InstanceSize.MEDIUM
+    //  ),
+    //  vpcSubnets: [
+    //    {
+    //      subnets: vpc.privateSubnets,
+    //    },
+    //  ],
+    //});
+    //cluster.addCdk8sChart(
+    //  "chart",
+    //  new NotisceChart(new cdk8s.App(), "notisce-chart")
+    //);
+    //const admin = User.fromUserName(this, "masterUser", "yushi");
+    //cluster.awsAuth.addUserMapping(admin, { groups: ["system:masters"] });
     //setupClusterLogging(this, cluster);
   }
 }
-const setupClusterLogging = (
-  stack: cdk.Stack,
-  cluster: FargateCluster
-): void => {
-  new AwsCustomResource(stack, "ClusterLogsEnabler", {
-    policy: AwsCustomResourcePolicy.fromSdkCalls({
-      resources: [`${cluster.clusterArn}/update-config`],
-    }),
-    onCreate: {
-      physicalResourceId: { id: `${cluster.clusterArn}/LogsEnabler` },
-      service: "EKS",
-      action: "updateClusterConfig",
-      region: stack.region,
-      parameters: {
-        name: cluster.clusterName,
-        logging: {
-          clusterLogging: [
-            {
-              enabled: true,
-              types: [
-                "chart",
-                "audit",
-                "authenticator",
-                "controllerManager",
-                "scheduler",
-              ],
-            },
-          ],
-        },
-      },
-    },
-    onDelete: {
-      physicalResourceId: { id: `${cluster.clusterArn}/LogsEnabler` },
-      service: "EKS",
-      action: "updateClusterConfig",
-      region: stack.region,
-      parameters: {
-        name: cluster.clusterName,
-        logging: {
-          clusterLogging: [
-            {
-              enabled: false,
-              types: [
-                "api",
-                "audit",
-                "authenticator",
-                "controllerManager",
-                "scheduler",
-              ],
-            },
-          ],
-        },
-      },
-    },
-  });
-};
+//const setupClusterLogging = (
+//  stack: cdk.Stack,
+//  cluster: FargateCluster
+//): void => {
+//  new AwsCustomResource(stack, "ClusterLogsEnabler", {
+//    policy: AwsCustomResourcePolicy.fromSdkCalls({
+//      resources: [`${cluster.clusterArn}/update-config`],
+//    }),
+//    onCreate: {
+//      physicalResourceId: { id: `${cluster.clusterArn}/LogsEnabler` },
+//      service: "EKS",
+//      action: "updateClusterConfig",
+//      region: stack.region,
+//      parameters: {
+//        name: cluster.clusterName,
+//        logging: {
+//          clusterLogging: [
+//            {
+//              enabled: true,
+//              types: [
+//                "api",
+//                "audit",
+//                "authenticator",
+//                "controllerManager",
+//                "scheduler",
+//              ],
+//            },
+//          ],
+//        },
+//      },
+//    },
+//    onDelete: {
+//      physicalResourceId: { id: `${cluster.clusterArn}/LogsEnabler` },
+//      service: "EKS",
+//      action: "updateClusterConfig",
+//      region: stack.region,
+//      parameters: {
+//        name: cluster.clusterName,
+//        logging: {
+//          clusterLogging: [
+//            {
+//              enabled: false,
+//              types: [
+//                "api",
+//                "audit",
+//                "authenticator",
+//                "controllerManager",
+//                "scheduler",
+//              ],
+//            },
+//          ],
+//        },
+//      },
+//    },
+//  });
+//};
